af18d54f4241a067f8c011d4db331177f233518c
sanathlokanath1991 <sanath.lokanath@collaberadigital.com>
2024-06-17 18:10:37 +0530

    Updating Variables File

--- variables.tf
+++ variables.tf
@@ -1,5 +1,24 @@
-variable "access_key" {}
-variable "secret_key" {}
-variable "awsRegion" {}
-variable "instanceType" {}
-variable "instanceName" {}
+variable "access_key" {
+  description = "The AWS access key"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "The AWS secret key"
+  type        = string
+}
+
+variable "awsRegion" {
+  description = "AWS Region for the instance"
+  type        = string
+}
+
+variable "instanceType" {
+  description = "AWS instance type"
+  type        = string
+}
+
+variable "instanceName" {
+  description = "Name of the AWS Instance"
+  type        = string
+}
