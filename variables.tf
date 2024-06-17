d4ae7e64d514c1793e538e6219dedbb8b3b88d6a
sanathlokanath1991 <sanath.lokanath@collaberadigital.com>
2024-06-17 19:40:23 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,22 +1,5 @@
-variable "aws_access_key_id" {
-  description = "AWS Access Key ID"
-}
-
-variable "aws_secret_access_key" {
-  description = "AWS Secret Access Key"
-}
-
-variable "awsRegion" {
-  description = "AWS Region for the instance"
-  type        = string
-}
-
-variable "instanceType" {
-  description = "AWS instance type"
-  type        = string
-}
-
-variable "instanceName" {
-  description = "Name of the AWS Instance"
-  type        = string
-}
+variable "access_key" {}
+variable "secret_key" {}
+variable "awsRegion" {}
+variable "instanceType" {}
+variable "instanceName" {}
