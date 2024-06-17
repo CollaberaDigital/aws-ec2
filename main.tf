5e6afe2cfb806b800c3e1e6b42e4cf9b5248a476
sanathlokanath1991 <sanath.lokanath@collaberadigital.com>
2024-06-17 19:16:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ terraform {
 
 provider "aws" {
   region     = var.awsRegion
-  access_key = var.access_key
-  secret_key = var.secret_key
+  access_key = env("AWS_ACCESS_KEY_ID")
+  secret_key = env("AWS_SECRET_ACCESS_KEY")
   #   profile = "sagar"
 }
 
